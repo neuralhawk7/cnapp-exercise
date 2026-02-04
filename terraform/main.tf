0f14730d9a9bc8756932839c19dc32f01c9803ed
neuralhawk7 <ramy@neuralhawk.com>
2026-02-03 21:24:37 -0800

    chore(terraform): restrict SSH ingress CIDR to 76.170.1.247/32

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,44 +1,31 @@
 
 ############################
-data "aws_vpc" "target" {
-  id = var.vpc_id
+# Create VPC (via module)
+############################
+module "vpc" {
+  source  = "./modules/vpc"
+  name    = var.name
+  cidr_block = var.vpc_cidr
+  public_subnets = var.public_subnet_cidrs
+  azs = length(var.azs) > 0 ? var.azs : slice(data.aws_availability_zones.available.names, 0, length(var.public_subnet_cidrs))
 }
+
+data "aws_availability_zones" "available" {}
 
 ############################
-# Internet Gateway + Public Subnet
+# EKS cluster (terraform-aws-modules/eks)
 ############################
-resource "aws_internet_gateway" "igw" {
-  vpc_id = data.aws_vpc.target.id
-  tags   = { Name = "${var.name}-igw" }
-}
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
 
-resource "aws_subnet" "public" {
-  vpc_id                  = data.aws_vpc.target.id
-  cidr_block              = var.public_subnet_cidr
-  availability_zone       = var.public_subnet_az
-  map_public_ip_on_launch = true
+  cluster_name    = var.cluster_name
+  cluster_version = "1.27"
 
-  tags = {
-    Name = "${var.name}-public-subnet"
-    # Not required for EC2, but harmless; helps if you ever use it for ELB
-    "kubernetes.io/role/elb" = "1"
-  }
-}
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
 
-resource "aws_route_table" "public" {
-  vpc_id = data.aws_vpc.target.id
-  tags   = { Name = "${var.name}-public-rt" }
-}
-
-resource "aws_route" "public_internet" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
+  # Managed node groups intentionally left out for now; add node groups as needed
 }
 
 ############################
@@ -67,14 +54,14 @@ data "aws_ami" "debian10" {
 resource "aws_security_group" "mongo_vm" {
   name        = "${var.name}-mongo-vm-sg"
   description = "Public SSH; Mongo only from VPC"
-  vpc_id      = data.aws_vpc.target.id
+  vpc_id      = module.vpc.vpc_id
 
   ingress {
     description = "SSH from Internet (required by assignment)"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.ssh_ingress_cidr]
   }
 
   ingress {
@@ -82,7 +69,7 @@ resource "aws_security_group" "mongo_vm" {
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
-    cidr_blocks = [data.aws_vpc.target.cidr_block] # 10.0.0.0/16
+    cidr_blocks = [module.vpc.vpc_cidr_block]
   }
 
   egress {
@@ -177,14 +164,14 @@ resource "aws_s3_bucket_policy" "public_read_and_list" {
 resource "aws_instance" "mongo" {
   ami                         = data.aws_ami.debian10.id
   instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.public.id
+  subnet_id                   = element(module.vpc.public_subnets, 0)
   vpc_security_group_ids      = [aws_security_group.mongo_vm.id]
   key_name                    = var.key_name
   associate_public_ip_address = true
 
   iam_instance_profile = aws_iam_instance_profile.mongo_vm_profile.name
 
-  user_data = templatefile("${path.module}/userdata.sh.tftpl", {
+  user_data = templatefile("${path.module}/mongo-vm/userdata.sh.tftpl", {
     mongo_admin_user = var.mongo_admin_user
     mongo_admin_pass = var.mongo_admin_pass
     backup_bucket    = aws_s3_bucket.backups.bucket
