7c81310e2a7407b8e864334f83fdc4d7176c3e21
neuralhawk7 <ramy@neuralhawk.com>
2026-02-06 08:55:13 -0800

    Remove duplicate EKS outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -47,18 +47,3 @@ output "eks_cluster_security_group_id" {
   description = "EKS cluster security group ID"
   value       = module.eks.cluster_security_group_id
 }
-
-output "eks_cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
-}
-
-output "eks_cluster_endpoint" {
-  description = "EKS cluster API endpoint"
-  value       = module.eks.cluster_endpoint
-}
-
-output "eks_cluster_security_group_id" {
-  description = "EKS cluster security group ID"
-  value       = module.eks.cluster_security_group_id
-}
