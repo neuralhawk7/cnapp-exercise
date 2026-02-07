65aa990988d4d2ae87bd0957da519ac9fae6b423
neuralhawk7 <ramy@neuralhawk.com>
2026-02-06 16:14:39 -0800

    Add WAF managed rules in count mode

--- terraform/main.tf
+++ terraform/main.tf
@@ -252,8 +252,30 @@ resource "aws_wafv2_web_acl" "main" {
   }
 
   rule {
-    name     = "count-all"
+    name     = "managed-common-rule-set"
     priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "managed-common-rule-set"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "count-all"
+    priority = 2
     action {
       count {}
     }
